fdc8c28dda7d22f2c6a26df8af82890f00a02f97
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-08 20:35:00 +0530

    Add files via upload

---
+++ GDTURN.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+    		int x = sc.nextInt();
+    		int y = sc.nextInt();
+    		if(x+y>6)
+		    System.out.println("yes");
+		    else
+		    System.out.println("no");
+		}
+		
+	}
+}
