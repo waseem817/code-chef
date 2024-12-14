c6d758f35b1b7a30784fc37615226a5e26a605a8
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-14 15:44:03 +0530

    Add files via upload

---
+++ PARLIAMENT.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new  Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0; i<t; i++)
+		{
+		    int n=sc.nextInt();
+		    int x=sc.nextInt();
+		    if(2*x>=n)
+		    {
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
