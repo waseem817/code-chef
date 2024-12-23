2514c19572a9ae56be630560d3f6f3f97f0942a1
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-23 15:49:51 +0530

    Add files via upload

---
+++ BATTERYLOW.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int t=0; t<n; t++)
+		{
+		    int x=sc.nextInt();
+		    if(x<=15)
+		    System.out.println("YES");
+		    else 
+		    System.out.println("NO");
+		    
+		}
+
+	}
+}
