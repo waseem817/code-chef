21c0da967aa0475d1a806941976b30bb4e46d7c9
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-23 15:54:46 +0530

    Add files via upload

---
+++ INCRIQ.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		    int x=sc.nextInt();
+		    int y=x+7;
+		    if(y>170)
+		    System.out.println("YES");
+		    else 
+		    System.out.println("NO");
+		    
+	}
+}
