d70b03b7fa614a26dae394047dd7e567ba81b94e
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-23 15:47:54 +0530

    Add files via upload

---
+++ CANCHEF.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int t=0; t<n; t++)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    if((x*15)>=(y*2))
+		    System.out.println("YES");
+		    else 
+		    System.out.println("NO");
+		    
+		}
+
+	}
+}
