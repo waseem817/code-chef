8bc27c4271b18269031d0527720c1e0ac0a31e2e
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-17 15:46:23 +0530

    Add files via upload

---
+++ TIMA.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner     sc=new  Scanner(System.in);
+		int x=sc.nextInt();
+		if((x+25)>=2050)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+
+	}
+}
