481097cc7b903e48af13a6f69d72daaa51468e18
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-17 15:29:39 +0530

    Add files via upload

---
+++ LUDO.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner     sc=new  Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x=sc.nextInt();
+		    if(x==6)
+		    System.out.println("yes");
+		    else
+		    System.out.println("no");
+		}
+
+	}
+}
