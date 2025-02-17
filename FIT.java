6f107a8a06b44f82dc62341f446073209e6a215d
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-17 15:55:33 +0530

    Add files via upload

---
+++ FIT.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        
+        while (t-- > 0) {
+            int x = scanner.nextInt();
+            System.out.println(x*10);
+            
+        }
+    }
+}
