79c6ad0aae026f40fbdcc255e4f23d05d44165cb
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-17 15:24:34 +0530

    Add files via upload

---
+++ BIRYANI.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    System.out.println(x*y);
+		}
+
+	}
+}
