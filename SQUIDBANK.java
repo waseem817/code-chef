b2b9b791fae4f0e657201ba61e9e0cd8ad915ccd
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-17 15:12:23 +0530

    Add files via upload

---
+++ SQUIDBANK.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner     sc=new  Scanner(System.in);
+	    int n=sc.nextInt();
+	    int k=sc.nextInt();
+	    int z=n-k;
+		System.out.println(z*10000);
+	}
+}
