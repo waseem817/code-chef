2db886446fec7f6d8f3d070e0182d33d2c550fea
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-19 19:23:50 +0530

    Add files via upload

---
+++ ELHP.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner  sc=new  Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		if(x>=(y*2))
+        System.out.println("yes");
+        else
+        System.out.println("no");
+	}
+}
