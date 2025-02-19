102c9b8e85b832b801cfd66f8cc2732071331c8f
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-19 18:13:38 +0530

    Add files via upload

---
+++ WATERCONS.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+    		int x = sc.nextInt();
+    		if(x>=2000)
+    		System.out.println("YES");
+    		else
+    		System.out.println("NO");
+		}
+		
+	}
+}
