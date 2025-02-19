5a4e6ca3eba687d41eb8a5c32df751cd80b26c51
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-19 11:18:07 +0530

    Add files via upload

---
+++ SWEETSHOP.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int n=sc.nextInt();
+		int r=x-(n*10);
+		System.out.println(r/20);
+
+	}
+}
