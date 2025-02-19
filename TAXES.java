b5fccf21394624fa8ccd865aad9d836dff1cef84
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-19 14:17:27 +0530

    Add files via upload

---
+++ TAXES.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x=sc.nextInt();
+		    if(x>100)
+		    System.out.println(x-10);
+		    else
+		    System.out.println(x);
+		}
+
+	}
+}
