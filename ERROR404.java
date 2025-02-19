38faea065f4c1664ac99c5a73c888828ecc7df11
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-19 18:35:55 +0530

    Add files via upload

---
+++ ERROR404.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new  Scanner(System.in);
+		int x=sc.nextInt();
+		if(x==404)
+        System.out.println("not found");
+        else
+        System.out.println("found");
+	}
+}
