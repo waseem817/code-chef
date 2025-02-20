a9237b5f40ec698414deb17ffdf6319fcb8e82c2
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-20 01:33:33 -0800

    Add files via upload

---
+++ DETSCORE.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner  sc=new  Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    int z=x/10;
+		    System.out.println(z*y);
+		}
+
+	}
+}
