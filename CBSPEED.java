008fdd18287dfc29ad10943bff3734ee86ead861
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-21 08:47:52 -0800

    Add files via upload

---
+++ CBSPEED.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		if(x<y)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+		
+	}
+}
