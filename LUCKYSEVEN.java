7b48293110c5098d980df250e5b1fa5023348126
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-22 20:51:08 -0800

    Add files via upload

---
+++ LUCKYSEVEN.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner  sc=new  Scanner(System.in);
+		String a=sc.next();
+		System.out.println(a.charAt(6));
+
+	}
+}
