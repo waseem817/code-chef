97d46a02423bdb4ebc225d0964a59280c60d3471
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-24 07:56:08 -0800

    Add files via upload

---
+++ SUMM.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner   sc=new  Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    int c=sc.nextInt();
+		    if(a+b==c)
+		    System.out.println("yes");
+		    else
+		    System.out.println("no");
+		}
+
+	}
+}
---
+++ TCG.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner  sc=new  Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		if(x<y)
+		System.out.println("INCREASED");
+		else if(x==y)
+		System.out.println("SAME");
+		else
+		System.out.println("DECREASED");
+
+	}
+}
