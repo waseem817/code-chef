37c37295e652a17a4e621f3a28f3de978d3c6c5d
waseem817 <s.shoaibwaseem786@gmail.com>
2025-02-28 08:36:03 +0530

    Paper Cutting.java

---
+++ Paper Cutting.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner  sc=new  Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int k=sc.nextInt();
+		    int n=sc.nextInt();
+		    System.out.println((k/n)*(k/n));
+		}
+
+	}
+}
