9b24706706635b0e8b0d2b301c95fea8a8a17ddd
waseem817 <s.shoaibwaseem786@gmail.com>
2025-03-03 15:26:34 +0530

    Add files via upload

---
+++ COUGAME.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner   sc=new   Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int g=sc.nextInt();
+		    int b=sc.nextInt();
+		    System.out.println(b-g);
+		}
+
+	}
+}
---
+++ SEMCOURSES.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner   sc=new   Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    int z=sc.nextInt();
+		    System.out.println(x*y*z);
+		}
+
+	}
+}
