970ad0aca656398413772164d8c86c41180f9830
waseem817 <s.shoaibwaseem786@gmail.com>
2025-03-03 15:47:46 +0530

    Add files via upload

---
+++ APPLORNG.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner  sc=new   Scanner(System.in);
+		int x=sc.nextInt();
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if(a+b<=x)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+
+	}
+}
