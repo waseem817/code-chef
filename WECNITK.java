81279f3f61de39fc6cc0ab89d179a7c61e3c4572
waseem817 <s.shoaibwaseem786@gmail.com>
2025-03-03 16:12:12 +0530

    Add files via upload

---
+++ WECNITK.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner   sc=new  Scanner(System.in);
+		String x="WECNITK";
+		String y=sc.next();
+		
+		if(x.equals(y))
+		System.out.println("Welcome to Web Club!");
+		else
+		System.out.println("Access denied");
+	}
+}
