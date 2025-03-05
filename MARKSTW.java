9468112470b5e1750921bf3260eae1b80a3a2b5f
waseem817 <s.shoaibwaseem786@gmail.com>
2025-03-05 17:56:46 +0530

    Add files via upload

---
+++ MARKSTW.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		if(x>=y*2)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+		
+	}
+}
