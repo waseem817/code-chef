ee22ef177a18fc1fa495753335c2b03e353afc2b
waseem817 <s.shoaibwaseem786@gmail.com>
2025-03-12 16:33:08 +0530

    Add files via upload

--- WAPEN.java
+++ WAPEN.java
@@ -6,10 +6,10 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner     sc=new  Scanner(System.in);
+		Scanner sc=new Scanner(System.in);
 		int x=sc.nextInt();
 		int y=sc.nextInt();
-		System.out.println(x+(y*10));
+		System.out.println(x+y*10);
 
 	}
 }
