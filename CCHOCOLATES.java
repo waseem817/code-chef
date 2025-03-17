e558e979fcfd78285f773a137458fa76652dbed6
waseem817 <s.shoaibwaseem786@gmail.com>
2025-03-17 16:14:31 +0530

    Add files via upload

---
+++ CCHOCOLATES.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            int z = scanner.nextInt();
+            System.out.println(((x*5)+(y*10))/z);
+        }
+    }
+}
---
+++ CWIREFRAME.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner  sc=new  Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++)
+		{
+		    int n=sc.nextInt();
+		    int m=sc.nextInt();
+		    int x=sc.nextInt();
+		    System.out.println(((2*n)+(2*m))*x);
+		}
+
+	}
+}
